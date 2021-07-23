5b46b74ea376380dc8fdaa15f53c11c6c366d009
namleyolo <happybear02042000@gmail.com>
2021-07-23 17:42:00 +0700

    first commit

---
+++ start/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+	"go.temporal.io/sdk/client"
+
+	"money-transfer-project-template-go/app"
+)
+
+// @@@SNIPSTART money-transfer-project-template-go-start-workflow
+func main() {
+	// Create the client object just once per process
+	c, err := client.NewClient(client.Options{})
+	if err != nil {
+		log.Fatalln("unable to create Temporal client", err)
+	}
+	defer c.Close()
+	options := client.StartWorkflowOptions{
+		ID:        "transfer-test",
+		TaskQueue: app.TransferMoneyTaskQueue,
+	}
+	transferDetails := app.TransferDetails{
+		Amount:      54.99,
+		FromAccount: "001-001",
+		ToAccount:   "002-002",
+		ReferenceID: uuid.New().String(),
+	}
+
+	we, err := c.ExecuteWorkflow(context.Background(), options, app.TransferMoney, transferDetails)
+	if err != nil {
+		log.Fatalln("error starting TransferMoney workflow", err)
+	}
+
+	var val  app.TransferDetails
+	err = we.Get(context.Background(), val)
+
+	fmt.Println("getValue",val)
+
+	printResults(transferDetails, we.GetID(), we.GetRunID())
+}
+// @@@SNIPEND
+
+func printResults(transferDetails app.TransferDetails, workflowID, runID string) {
+	log.Printf(
+		"\nTransfer of $%f from account %s to account %s is processing. ReferenceID: %s\n",
+		transferDetails.Amount,
+		transferDetails.FromAccount,
+		transferDetails.ToAccount,
+		transferDetails.ReferenceID,
+	)
+	log.Printf(
+		"\nWorkflowID: %s RunID: %s\n",
+		workflowID,
+		runID,
+	)
+}
